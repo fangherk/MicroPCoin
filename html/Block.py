e1f07174340f2061b8f6c7a41ba098167aa3a684
mjenrungrot <mjenrungrot@hmc.edu>
2017-11-19 04:21:18 +0000

    Fix block.py

--- html/Block.py
+++ html/Block.py
@@ -8,10 +8,11 @@ class Block:
         self.hash = ""                  # hash taken from the contents of the block:
                                         # sha256(index + previousHash + timestamp + nonce + transactions)
 
-    def getGenesis(self):
-        self.index = 0
-        self.previousHash = "0" * 64
-        self.timestamp = 1465154705
-        self.nonce = 0
-        self.transactions = []
-        return self 
+def getGenesis():
+    block = Block()
+    block.index = 0
+    block.previousHash = "0" * 64
+    block.timestamp = 1465154705
+    block.nonce = 0
+    block.transactions = []
+    return block 
