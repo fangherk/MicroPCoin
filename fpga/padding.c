d817474af27a778f9a70453f3bada9b4152aaf6e
Herrick Fang <herrickfang@gmail.com>
2017-11-28 21:22:06 +0000

    Mek's shortened padding

--- fpga/padding.c
+++ fpga/padding.c
@@ -4,44 +4,45 @@
 // padding.c section 5.1.1
 
 #include <stdio.h>
-
+#include <string.h>
 unsigned int findK(unsigned int l){
-  // Find k to satisfy the equivalence relation:
-  // l+1+k === 448 mod 512
+    return (((448 - l -1) % 512) + 512) % 512;
+}
 
-  unsigned int total = l + 1;
-  unsigned int k;
-  
-  // Deal with the less than 448 case
-  if (total < 448){
-    k = 448 - total;
-  }else{
-    // Deal with > 448 case 
-    unsigned modded = total % 512;
-    if(modded > 448 && modded < 512){
-      // go to the next mod value up
-      k = 960 - modded;
-    }else{ // modded < 512
-      // just add up to 448 
-      k = 448 - modded;
+unsigned long long swapBytesOrder(unsigned long long val){
+    unsigned long long answer = 0;
+    unsigned char buffer[20];
+    int i,j;
+    for(i=0;i<8;i++){
+        buffer[i] = (val & (0xFFULL << (i << 3))) >> (i << 3);
     }
-  }
-  return k;
+    for(i=7,j=0;i>=0;i--,j++){
+        answer |= ((unsigned long long)buffer[i] << (j << 3));
+    }
+    return answer; 
 }
 
 int main()
 {
-  // Add the python message here.
-  unsigned char msg[2048] = "abc";
-  unsigned int i, l=0;
 
+  unsigned char output[2048];
+  memset(output, 0, sizeof(output));
+  // Add the python message here.
+//  unsigned char msg[2048] = "abc";
+  unsigned char msg[2048] = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu";
+  unsigned int i;
+  unsigned long long l=0LL;
   // Generate the ith value of the char array
   // Gather the number of char bits as well
   i = 0;
+
   while(msg[i] != '\0'){
-    l+=8;
+    output[i] = msg[i];
+    output[i+1] = 0x80;
+    l+=8LL;
     i++;
   }
+
   printf("i: %d, l: %d", i, l);
 
   
@@ -49,55 +50,15 @@ int main()
   // to satisfy l + 1 + k === 448 mod 512
   unsigned int k = findK(l);
   printf("l: %d\nk: %d\nl+1+k: %d\nl+1+k mod 512: %d\n",l,k,l+1+k, (l+1+k)%512);
+  printf("%x %lld\n",l, l);
+  printf("%lx\n", swapBytesOrder(l)); 
+  unsigned long long* lengthPosition = (unsigned long long*)(output + ((l + 1 + k) >> 3));
+  *lengthPosition = swapBytesOrder(l);
 
-
-  // Add a 1 bit to the end of the message.
-  // Assume k is larger than 7. Is this a safe assumption?
-  // Then, we should get
-  unsigned char tmp1 = 0x80;
-  unsigned char tmp0 = 0x00;
-  strncat(msg, &tmp1, 1);
-  i++;
-  
-
-  // Append k zeros
-  int leftovers = (k+1) - 8;
-  int z;
-  int limitPt = leftovers/8;
-  if (k != 7){
-    // printf("leftovers %d\n", limitPt);
-    while( z != limitPt){
-      printf("z: %d\n", z);
-      msg[i] = tmp0;
-      i++;
-      z++;
-    }
-  }
-
- 
-  // Append 64 bits to the end
-  unsigned char temp64[8];
-  unsigned long long appendBits = l;
-  unsigned int j;
- 
-  for(j=0; j < 8; j++){
-    temp64[j] = (appendBits << (j*8)) & 0xFF;
-  }
-
-  for(j=0; j < 8; j++){
-    msg[i] = temp64[7-j];
-    i++;
-  }
-
-  // Add the last terminating string
-  msg[i] = '\0';
-  // possibly off by 1? Should we increase by 1
-
-  //Print out the resulting value
   
   unsigned int p = 0;
-  while(p < i){
-    printf("\nmsg %d: %02x", p, msg[p]);
+  while(p < ((l+1+k+64)>>3)){
+    printf("%02x", output[p]);
     p++;
   }
   
