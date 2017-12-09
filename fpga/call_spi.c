116e961368d89567aa85c63e2287ee987f3f347b
Herrick Fang <herrickfang@gmail.com>
2017-12-08 20:19:51 -0800

    Commenting and cleaning updates

--- fpga/call_spi.c
+++ fpga/call_spi.c
@@ -1,7 +1,6 @@
 // spi.c
-// hfang@hmc.edu, mjenrungrot@hmc.edu 24 November 2017
+// hfang@hmc.edu, mjenrungrot@hmc.edu 8 December 2017
 // Send data to hardware accelator using SPI
-
 
 // Libraries
 #include <stdio.h>
@@ -9,10 +8,12 @@
 #include <stdlib.h>
 #include "EasyPIO.h"
 
+// Find k
 unsigned int findK(unsigned int l){
     return (((448 - l -1) % 512) + 512) % 512;
 }
 
+// Swap Big + Little Endian
 unsigned long long swapBytesOrder(unsigned long long val){
     unsigned long long answer = 0;
     unsigned char buffer[20];
@@ -26,12 +27,12 @@ unsigned long long swapBytesOrder(unsigned long long val){
     return answer; 
 }
 
+// Pad a given input to create input_message
 void padding(unsigned char *input, int nBytes, unsigned char* output, int *len){
     unsigned int inputLength = nBytes * 8;
     unsigned int k = findK(inputLength);
     unsigned int outputLength = inputLength  + 1+ k + 64;
-    //printf("output length: %d\n", outputLength);
-    //printf("input length: %d\n", inputLength);
+
     outputLength /= 8;
     memset(output, 0, sizeof(output));
 
@@ -52,6 +53,7 @@ void padding(unsigned char *input, int nBytes, unsigned char* output, int *len){
 
 unsigned char difficultyBlock[64];
 
+// Generate the difficulty from the terminal input
 void generateDifficulty(unsigned int arg1){
     // convert bytes order
     unsigned short byte1 = (arg1 & 0xff000000);
@@ -72,56 +74,55 @@ void generateDifficulty(unsigned int arg1){
 #define INPUT_RDY_PIN 17
 
 // Function prototypes
-void encrypt(unsigned char*, unsigned char*);
-void printNum(unsigned char*, int);
-//void printall(char*, char*);
-
 unsigned char msg[2000000];
 unsigned char output[2000000];  
 unsigned int arg1;
+
+
 // Main
 int main(int argc, char *argv[]){
+
+    // Create a place to store each block   
     unsigned char key[64];
+
+    // Prepare the message by setting it to 0s
     memset(msg, 0, sizeof(msg));
 
-    // read bytes
+    // Read the input message through a text file
     FILE *fread = fopen("input_message.txt", "rb");
     fseek(fread, 0, SEEK_END);
+
+
+    // Start the counter and sizes
     int lsize, idxCounter = 0;
     lsize = ftell(fread);
     rewind(fread);
+
+
+    // Wait for the counter to be less than the size
     while(idxCounter < lsize){
         int first = fgetc(fread);
         msg[idxCounter] = (unsigned)first;
-    //    printf("%02x", msg[idxCounter]);
         idxCounter++;
     }
-    //printf("\n");
 
 
-    // get difficulty block
+    // Generate a difficulty block to send to the FPGA
     memset(difficultyBlock, 0, sizeof(difficultyBlock));
     arg1 = atoi(argv[1]);
     generateDifficulty(arg1);
 
     fclose(fread);
-    //printf("nBytes = %d\n", lsize);
 
+    // Pad the message according to FIPS
     int paddingLength, i, nblock=0;  
     padding((unsigned char *)msg, lsize, output, &paddingLength);
-    //printNum(output, 192);
-    
     int nBlocks = paddingLength / 64;
-
     int block;
-    /*for(block=0; block<nBlocks;block++){
-        printf("Block %d\n", block);
-        for(i=0;i<64;i++) printf("%02x", output[64*block + i]);
-        printf("\n");
-    }*/
 
+    // Initialize pins for blocks
     pioInit();
-    spiInit(6000, 0);
+    spiInit(150000, 0);
     pinMode(MSG_PIN, OUTPUT);
     pinMode(BLOCK_PIN, OUTPUT);
     pinMode(LOAD_PIN, OUTPUT);
@@ -132,16 +133,17 @@ int main(int argc, char *argv[]){
     unsigned char nonce[4];
     memset(sha256, 0, sizeof(sha256));
 
-        
 
-
+    // Start the block sending process. 
+    // Start with high message and load pins.
+    // Turn off the load pin and continue after the
+    // first couple blocks. Continue sending blocks
+    // and sending on/off signals accordingly.   
 
     for(block=0; block<nBlocks;block++){
         for(i=0;i<64;i++){
             key[i] = output[64*block + i];
-      //      fprintf(stderr, "%02x", key[i]);
         }
-    //    fprintf(stderr, "\n");
         if(block == 0){
             digitalWrite(MSG_PIN, 1);
             digitalWrite(LOAD_PIN, 1);
@@ -157,24 +159,30 @@ int main(int argc, char *argv[]){
             while(!digitalRead(INPUT_RDY_PIN));
         }
     }
-//    fprintf(stderr, "Difficulty block\n");
+
+    // Send the last difficulty block
     for(i=0;i<64;i++){
         key[i] = difficultyBlock[i];
-  //      fprintf(stderr, "%02x", key[i]);
     }
     digitalWrite(BLOCK_PIN, 1);
     for(i=0; i< 64; i++)  spiSendReceive(key[i]);
     digitalWrite(BLOCK_PIN, 0);           
     digitalWrite(MSG_PIN, 0);
 
+    // End the output and wait for the response from the FPGA
     while (!digitalRead(DONE_PIN));
+
+    // Get the Sha256 output hash
     for(i=0; i< 32; i++){
         sha256[i] = spiSendReceive(0);
     }
+
+    // Get the nonce value
     for(i=0; i<4;i++){
         nonce[i] = spiSendReceive(0);
     }
 
+    // Parse the nonce vlaue and output to a file, then output the sha256 value
     int nonceValue = (nonce[0] << 24) | (nonce[1] << 16) | (nonce[2] << 8) | nonce[3];
 
     FILE *fp = fopen("output.txt", "w");
@@ -183,46 +191,8 @@ int main(int argc, char *argv[]){
         fprintf(fp, "%02x", sha256[i]);
     }
     fclose(fp);
-    //sleep(1);
-
-    // printNum(sha256, 32);
-    // printall(key, sha256);
-   // printall(key2, sha256);
     return 0;
 }
 
-
-//Functions
-void printall(unsigned char *key, unsigned char *sha256){
-//    printf("Key:         "); printNum(key, 64);
-//    printf("Sha256:      "); printNum(sha256, 32);
-//    printf("Expected:    "); printf("11111011011001111000111100110000001111000101111010110100001001111011010010110001100000001010000111111100010100111010111010010001\n");
-//    //printNum(expected, 32);
-//
-//    if(strcmp(expected, sha256) == 0){
-//        printf("\nSuccess!\n");
-//    }else{
-//        printf("\n Test Failed. Saadddd");
-//    }
-}
-
-void encrypt(unsigned char *key, unsigned char *sha256){
-    int i;
-    int j = 0;
-    int ready;
-    for(i = 0; i < 64; i++){
-        spiSendReceive(key[i]);
-    }
-}
-
-void printNum(unsigned char *text, int num){
-    int i;
-
-    for(i = 0; i < num; i++){
-        // printf("%02x", text[i]);
-        printf("%d%d%d%d",(text[i]&8) > 0, (text[i]&4)>0, (text[i]&2)>0, text[i]&1);
-    }
-    printf("\n"); 
-}
 
 
