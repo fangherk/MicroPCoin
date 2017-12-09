d9ceb4e2929dae038bb76a735b1447544a7f3547
Herrick Fang <herrickfang@gmail.com>
2017-12-08 20:52:53 -0800

    added header and removed prints

--- html/hashing.py
+++ html/hashing.py
@@ -1,9 +1,16 @@
+# hashing.py
+# HMC E85 8 December 2017
+# hfang@hmc.edu, mjenrungrot@hmc.edu
+
 import subprocess
 import time
 ITER = 15 
 
 
+
 def get_spi(input_msg=None, difficulty=0):
+    """Get the SPI output by running the C algorithm """
+    # Set the input message
     if input_msg is not None:
         with open("input_message.txt", "wb") as f:
             f.write(input_msg)
@@ -12,19 +19,23 @@ def get_spi(input_msg=None, difficulty=0):
     previousHash = None
     i = 0
     counter = 0
+
+    # Run the hashing process several times to gather the correct hash output from the C program
     while i  < ITER:
+        # Run the C modules
         try:
             subprocess.run(["sudo", "./call_spi", str(difficulty)], timeout=2)
         except subprocess.TimeoutExpired: 
             pass 
-        # print("Block {:} is done".format(i))
+        
         time.sleep(0.2)
+
+        # Read the output from the C program
         with open("output.txt", "r") as f2:
             outputNonce = int(f2.readline())
             outputHash = f2.readline()
 
-        print(outputNonce)  
-        print(outputHash)
+        # Wait until we have three of the same values to break
         if outputHash == previousHash:
             counter += 1
         else:
@@ -40,15 +51,11 @@ def get_spi(input_msg=None, difficulty=0):
         f2.close()
         i += 1
 
+    # Return the actual hash and nonce from what the most common ones before
     (actual_hash, actual_nonce) = max(set(hashes), key =hashes.count)
-    print("Actual Hash: {:}    nonce = {:}".format(outputHash, outputNonce))
+   
     return  (actual_hash, actual_nonce)
 
-def main():
-    get_spi()
-
-if __name__  == "__main__":
-    main()
 
 
 
